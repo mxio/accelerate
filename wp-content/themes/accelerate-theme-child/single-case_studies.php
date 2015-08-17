f5810182a2ddbe8c8e890ce6a244dbc692463ac8
danyiceandfire <maitongxiong@gmail.com>
2015-08-17 18:06:20 -0500

    update case study

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -17,9 +17,11 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
+				<article class="case-study">
 				<?php the_content(); ?>
+				</article>
 			<?php endwhile; // end of the loop. ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
-<?php get_footer(); ?
+<?php get_footer(); ?>
