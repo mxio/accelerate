3d04b4332ad6e7bf193a22040d6ebe159ce5a589
danyiceandfire <maitongxiong@gmail.com>
2015-08-17 19:16:53 -0500

    acf

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,11 +14,34 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
+<div id="primary" class="site-content">
+	<div id="content" role="main">
+		<?php while ( have_posts() ) : the_post(); ?>
+			$services = get_field('services');
+			$client = get_field('client');
+			$link = get_field('site_link');
+			$image_1 = get_field('image_1');
+			$image_2 = get_field('image_2');
+			$image_3 = get_field('image_3'); ?>
+
 				<article class="case-study">
+					<aside class="text">
+					<h2><?php the_title(); ?></h2>
+					<h5><?php echo $services; ?> </h5>
+					<h6>Client: <?php $client; ?></h6>h6
+				
 				<?php the_content(); ?>
+
+				<p><a href="<?php echo $link; ?>">Site Link</a></p>
+
+			</aside>
+
+	<div class="images">
+	<img src="<?php echo $image_1; ?>" />
+	<img src="<?php echo $image_2; ?>" />
+	<img src="<?php echo $image_3; ?>" />
+</div>
+
 				</article>
 			<?php endwhile; // end of the loop. ?>
 
