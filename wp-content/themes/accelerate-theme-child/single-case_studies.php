482bcd8ac483d744c206e26353b4927d3a8e8c13
danyiceandfire <maitongxiong@gmail.com>
2015-08-17 19:19:18 -0500

    fix errors

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 <div id="primary" class="site-content">
 	<div id="content" role="main">
-		<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post();
 			$services = get_field('services');
 			$client = get_field('client');
 			$link = get_field('site_link');
