fc2640a4c150dd0eb52a72eca6cefbe457e68a65
danyiceandfire <maitongxiong@gmail.com>
2015-08-17 20:35:17 -0500

    include proper php tag

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -29,7 +29,7 @@ get_header(); ?>
 
 	<h4>Featured Work</h4>
 
-	?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+	<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
  			<?php while ( have_posts() ) : the_post(); 
  				$image_1 = get_field("image_1");
  				$size = "medium";
