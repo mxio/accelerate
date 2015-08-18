7c2ffc9914c5c566e3fff4ecd44cf9a0ea4f1f8c
danyiceandfire <maitongxiong@gmail.com>
2015-08-17 20:58:05 -0500

    add archive page

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * The template for displaying all case studies archive page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); 
+				$image_1 = get_field('image_1');
+				$size = "medium"; ?>
+				$services = get_field('services');
+			?>
+
+	<aside class="text">
+					<h2><a href="<?php the_permalink(); ?>"><?php the_title; ?></a></h2>
+					<h5><?php echo $services; ?> </h5>
+				
+				<?php the_excerpt(); ?>
+
+				<p><a href="<?php the_permalink(); ?>">View Project</a></p>
+
+			</aside>
+
+	<div class="images">
+	<?php if($image_1) { ?>
+	<?php echo wp_get_attachment_image($image_1, $size); ?>
+	<?php } ?>
+
+</div>
+
+				</article>
+
+
+				<?php the_excerpt(); ?>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
